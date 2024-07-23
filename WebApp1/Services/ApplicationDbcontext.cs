b8945273e16dd496d1725972716f1cfa5d34bcff
harini1817 <harinisree2023@gmail.com>
2024-07-23 17:08:38 +0530

    Add project files.

---
+++ WebApp1/Services/ApplicationDbcontext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp1.Models;
+
+namespace WebApp1.Services
+{
+    public class ApplicationDbcontext : DbContext
+    {
+        public ApplicationDbcontext(DbContextOptions options) : base(options) {
+        }
+        public DbSet<Product> Products {get; set;}
+    }
+}
